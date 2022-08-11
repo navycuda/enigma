9a89d3d5277ff739ec22d15f40c783076b9dae5b
Shane <navysupra@gmail.com>
2022-08-11 17:47:07 +0000

    building out the rotor wiring

--- modules/Rotor.js
+++ modules/Rotor.js
@@ -1,3 +1,31 @@
+const rotorWiring = {
+  I: {
+    wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+    notch: "Y",
+    turnover: "Q"
+  },
+  II: {
+    wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+    notch: "M",
+    turnover: "E"
+  },
+  III: {
+    wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+    notch: "D",
+    turnover: "V"
+  },
+  IV: {
+    wiring: "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+    notch: "R",
+    turnover: "J"
+  },
+  V: {
+    wiring: "VZBRGITYUPSDNHLXAWMJQOFECK",
+    notch: "H",
+    turnover: "Z"
+  },
+};
+
 class Rotor {
   constructor() {
 
--- modules/etw.js
+++ modules/etw.js
@@ -1,4 +1,4 @@
-const cipherChars = 'ABCDEFGHIJKLMNOPQRSTUV';
+const cipherChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 
 module.exports = {
